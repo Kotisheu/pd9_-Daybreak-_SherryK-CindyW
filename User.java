8f493102d7c9035a86d385a667d2799ef25ba145
Cindy <cindyofwu@gmail.com>
2014-05-31 12:32:07 -0400

    Basic things a user has

--- User.java
+++ User.java
@@ -1,8 +1,42 @@
-public class User {
+import java.io.*;
+import java.util.ArrayList;
+
+public class User extends Game {
     
-    private ArrayList<Base> _avail = new ArrayList<Base>(); //available team members
-    private Base[] _team = new Base[4]; //the user's team in battle (up to 4);
+    private ArrayList<Base> _avail; //available team members
+    private ArrayList<Base> _team; //the user's team in battle (up to 4);
     
-    public Base[] addTeam( Base p ) {
-	if ( _team.length == 4 ) {
-	    swap( p
+    public User() {
+	_avail = new ArrayList<Base>();
+	_team = new ArrayList<Base>();
+    }
+
+    public ArrayList<Base> addTeam( int i ) {
+	if ( _team.length < 4 ) {
+	    //add person to team and remove from list of available
+	    _team.add( i );
+	    _avail.remove( i );
+	}
+	return _team;
+    }
+
+    public Base remTeam( int i ) {
+	//return removed person; remove from team and add to list of available
+	if ( _team.length != 1 )
+	    return _avail.add( _team.remove(i) );
+
+	return null;
+    }
+
+    public ArrayList<Base> getTeam() {
+	return _team;
+    }
+    
+    public ArrayList<Base> getAvail() {
+	return _avail;
+    }
+
+}
+
+
+			 
