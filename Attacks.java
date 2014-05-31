3603f528a87790085997dc547f407288bc2eae13
Cindy <cindyofwu@gmail.com>
2014-05-31 12:33:03 -0400

    Reminder for attacks

--- Attacks.java
+++ Attacks.java
@@ -1,15 +1,26 @@
+//**NOTE: attacks under this class should have a constructer specifying target and attacker (and other stuff listed in Attacks variables). Has method Declare to use norm attack (uses str) or mag attack(uses wis) when popped out of heap.
+
 public class Attacks {
     
+    private int _pri; //how long it takes to use attack
+    private int _multiplier; //damage multiplier
+    private int _mag; //mp used
+    private Base _tar;
+    private Base _atkr;
+
     //People will have certain attacks in their attack ArrayList based on their class
 
     public int Norm ( Base tar, Base atkr ) {
 	//values will probably change b/c of whatever values we use
-	//defense is 1/4defense + a random range 0def - .5def
+	//strength of atk depends on attack
+	//defense is 1/2defense + random range 1-4;
 	int dam = 
-	    atkr.getAtk() - 
-	    ( tar.getDef()*.25 + tar.getDef() * (Math.random()*.5) );
+	    atkr.getAtk()*_multiplier - 
+	    ( tar.getDef()*.5 + Math.random()*4 );
 	
-	tar.setHp ( tar.getHp() - dam );
+	atkr.setMp ( atkr.getMp() - _mag ); //lose mp
+
+	tar.setHp ( tar.getHp() - dam ); //lose hp
 
 	check(tar);
 	/*::::::INPUT CHECK FOR TARGET DEAD OR NOT::::::
@@ -18,28 +29,29 @@ public class Attacks {
 	*/
 	return dam;
     }
-
-    //This attack is a little stronger than Norm
-    public int A1 ( Base tar, Base atkr ) {
-	int dam = atkr.getDef()*1.25 - 
-	    ( tar.getDef()*.25 + tar.getDef() * (Math.random()*.5) );
-	
-	atkr.setMp ( tar.getMp() - 2 ); //This attack uses mp
-	
-	tar.setHp ( tar.getHp() - dam ); 
-	
-	check(tar);
-	return dam;
-    }
       
-    public int MagR ( Base tar, Base atkr ) {
-	int dam = atkr.getWis() - 
-	    ( tar.getDef()*.25 + tar.getDef() * (Math.random()*.5) );
+    public int Mag ( Base tar, Base atkr ) {
+	int dam = atkr.getWis()*_multiplier - 
+	    ( tar.getDef()*.5 + Math.random()*4 );
 	
-	atkr.setMp ( tar.getMp() - 2 ); //This attack uses mp
+	atkr.setMp ( tar.getMp() - _mag ); //use mp
 
-	tar.setHp ( tar.getHp() - dam );
+	tar.setHp ( tar.getHp() - dam ); //lose hp
 	
 	check(tar);
 	return dam;
     }
+
+    public int getPri() {
+	return _pri;
+    }
+
+    public Base getUser() {
+	return _atkr;
+    }
+    
+    public Base getTar() {
+	return _tar;
+    }
+
+}
