7916d3a6bca0b1fd875a7f032e1f637c42641704
Kotisheu <stariecherrie@Gmail.com>
2014-06-04 14:41:21 -0400

    rarw

---
+++ .#Status.java
@@ -0,0 +1,1 @@
+sherry.ko@cslab3-28.stuy.edu.6231:1401808521
--- Area.java
+++ Area.java
@@ -3,31 +3,70 @@ import java.util.ArrayList;
 public abstract class Area{
     protected ArrayList<Item> store;//what items are availiable in this place
     protected ArrayList<Monster> munstars;// what monsters are here
-    protected ArrayList<Trivia> people;// trivia given by talking to random people in towns :)
+    protected ArrayList<Trivia> citizens;// trivia given by talking to random people in towns :)
     protected boolean first; // is this first time here?- trigger event
     protected String name;
     protected boolean town;//true if town, false if outside area
-    protected int step;//step this is at <town>, starting step for area
+    protected int step;//step this is 0 at <town>, starting step for area
     protected int stepstowards;// steps to town in front
     protected int stepsbackwards;//steps to town behind
+    protected Area nextplace;
+    protected Area backplace;
     
     public ArrayList<Monster> getMon() {
 	return munstars;
     }
-}
-//Wait this should be in a different file? or no...
-// class Village extends Area{
-//     //I just fleshed this our so i could try testing....
-//     public Village() {
-// 	first = true;
-// 	town = true;
-// 	munstars = new ArrayList<Monster>();
-// 	store = new ArrayList<Item>();
-// 	people = new ArrayList<Trivia>();
-// 	step = 0;
-// 	stepstowards = 10;
-// 	stepsbackwards = 0;
-// 	System.out.println( "random story?" );
-//     }
+    public Area MoveFoward(){
+	if (town){
+	    return nextplace;
+	}
+	else{ 
+	    step+=1;
+	    check();
+	}
+    }
+    public abstract Area Check();
 
-// }
+}
+
+class Village extends Area{
+    public Village() {
+	first = true;
+	town = true;
+	name= "Pellet Town";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	System.out.println( "random story?" );
+	nextplace=new Grassland();
+	backplace=null;
+    }
+    public Area Check(){//town no need to do anything
+    }
+
+}
+class Grassland extends Area{
+    public Grassland(){
+	first = true;
+	town= false;
+	name= "Stonehenge";
+	munstars = new ArrayList<Monster>();
+	store = null;//noneedforstore
+	citizens = null; // nopeople
+	step = 1;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	System.out.println( "random story?" );
+	nextplace=new Kaetown();
+	backplace=village();
+    }
+    public Area Check(){
+	if (step == stepstowards){
+	}
+	if(step==stepsbackwards){
+	}
+    }
+}
