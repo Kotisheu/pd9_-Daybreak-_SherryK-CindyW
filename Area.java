dfc27f695d06f8bc0eab5210cf63e2d942fdde84
Kotisheu <stariecherrie@Gmail.com>
2014-06-09 14:43:53 -0400

    Areaed

--- Area.java
+++ Area.java
@@ -1,5 +1,4 @@
 //EDIT TEMPERARY RETURN STATEMNTS AT MOVEFORWARD AND CHECK() METHODS
-//ALSO LOOK AT LINES 20-26 BECAUSE LEFT YOU A MESSAGE =w=
 
 import java.util.ArrayList;
 
@@ -15,16 +14,19 @@ public abstract class Area{
     protected int stepsbackwards;//steps to town behind
     protected Area nextplace;
     protected Area backplace;
-  
-    /*
-      SHERRY
-      I think there should be variables for these, since we shouldnt be creating new areas when they go back. Right? Im not sure how you want to do this, but originally you had backplace = Village(), which doesnt work because it should be new Village(), but i dont know if we want to create a new one. 
-IDK.Stuff works now so yey?
-    */
-    protected static Village village = new Village();
-    protected static Grassland grassland = new Grassland();
-    protected static Kaetown kaetown = new Kaetown();
-    //^^
+ 
+    protected static Area pellet = new Village();
+    protected static Area stonehenge = new Grassland();
+    protected static Area kaetown = new Townuno();
+    protected static Area feywood = new Forest();
+    protected static Area seaporte= new Towndos();
+    protected static Area aguael= new Water(); 
+    protected static Area towntree= new Towntres();
+    protected static Area sans= new Mountain(); 
+    protected static Area metro= new Towncuatro();
+    protected static Area dessert = new Desert();
+    protected static Area tower= new Tower();
+
 
     public ArrayList<Monster> getMon() {
 	return munstars;
@@ -35,11 +37,10 @@ IDK.Stuff works now so yey?
 	}
 	else{ 
 	    step+=1;
-	    Check();
+	    return Check();
 	}
 
-	//TEMPORARY RETURN STATEMENT
-	return null;
+	return this;
     }
     public abstract Area Check();
 
@@ -56,7 +57,156 @@ IDK.Stuff works now so yey?
 
 class Village extends Area{
     public Village() {
-	first = true;
+	first = false;//if false, trigger event, change to true
+	town = true;
+	name= "Pellet Town";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace= stonehenge;
+	backplace=null;
+    }
+    public Area Check(){//town no need to do anything
+	return this;
+    }
+
+}
+class Grassland extends Area{
+    public Grassland(){
+	first = false;
+	town= false;
+	name= "Stonehenge";
+	munstars = new ArrayList<Monster>();
+	store = null;//noneedforstore
+	citizens = null; // nopeople
+	step = 1;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace=kaetown;
+	backplace=pellet;
+    }
+    public Area Check(){
+	if (step == stepstowards){
+	}
+	if(step==stepsbackwards){
+	}
+	return this;
+    }
+}
+class Townuno extends Area{
+    public Townuno(){
+	first = false;//if false, trigger event, change to true
+	town = true;
+	name= "Kaetown";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace=feywood;
+	backplace=stonehenge;
+
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Forest extends Area{
+    public Forest(){
+	town = false;
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Towndos extends Area{
+    public Towndos(){
+	first = false;//if false, trigger event, change to true
+	town = true;
+	name= "Seaporte";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace= aguael;
+	backplace=feywood;
+
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Water extends Area{
+    public Water(){
+	town=false;
+    }
+    public Area Check(){
+	return this;	
+    }
+}
+class Towntree extends Area{
+    public Towntree(){
+	first = false;//if false, trigger event, change to true
+	town = true;
+	name= "Pellet Town";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace= grassland;
+	backplace=null;
+
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Sans extends Area{
+    public Sans(){
+	town= false;
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Metro extends Area{
+    public Metro(){
+	first = false;//if false, trigger event, change to true
+	town = true;
+	name= "Pellet Town";
+	munstars = new ArrayList<Monster>();
+	store = new ArrayList<Item>();
+	citizens = new ArrayList<Trivia>();
+	step = 0;
+	stepstowards = 10;
+	stepsbackwards = 0;
+	nextplace= grassland;
+	backplace=null;
+
+    }
+    public Area Check(){
+	return this;
+    }
+}
+class Dessert extends Area{
+    public Dessert(){
+	town=false;
+    }
+    public Area Check(){	
+	return this;
+    }
+}
+class Tower extends Area{
+    public Tower(){
+	first = false;//if false, trigger event, change to true
 	town = true;
 	name= "Pellet Town";
 	munstars = new ArrayList<Monster>();
@@ -68,113 +218,8 @@ class Village extends Area{
 	nextplace= grassland;
 	backplace=null;
     }
-    public Area Check(){//town no need to do anything
-	//TEMPORARY RETURN 
-	return null;
-    }
-
-}
-class Grassland extends Area{
-    public Grassland(){
-	first = true;
-	town= false;
-	name= "Stonehenge";
-	munstars = new ArrayList<Monster>();
-	store = null;//noneedforstore
-	citizens = null; // nopeople
-	step = 1;
-	stepstowards = 10;
-	stepsbackwards = 0;
-	nextplace=kaetown;
-	backplace=village;
-    }
     public Area Check(){
-	if (step == stepstowards){
-	}
-	if(step==stepsbackwards){
-	}
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Kaetown extends Area{
-    public Kaetown(){
-	town = true;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Feywood extends Area{
-    public Feywood(){
-	town = false;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Seaporte extends Area{
-    public Seaporte(){
-	town = true;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Aguael extends Area{
-    public Aguael(){
-	town=false;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;	
-    }
-}
-class Towntree extends Area{
-    public Towntree(){
-	town=true;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Sans extends Area{
-    public Sans(){
-	town= false;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Metro extends Area{
-    public Metro(){
-	town= true;
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Dessert extends Area{
-    public Dessert(){
-	town=false;
-    }
-    public Area Check(){	
-	//TEMPORARY RETURN 
-	return null;
-    }
-}
-class Tower extends Area{
-    public Tower(){
-    }
-    public Area Check(){
-	//TEMPORARY RETURN 
-	return null;
+	return this;
     }
 }
 
