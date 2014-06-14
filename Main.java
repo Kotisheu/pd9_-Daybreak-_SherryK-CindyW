d613d5c3ee815325378d1a48f75b38479f4b69e8
Cindy <cindyofwu@gmail.com>
2014-06-14 17:14:43 -0400

    also edited slightly for testing purposes

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main extends People{
     public Main(){
+	setPlace( new Village() );
+	bhp = hp = 15;
     }
     public Main(String n){
+	this();
 	name=n;
-	setPlace( new Village() );
     }
 }
