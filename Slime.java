0fa087b5d3c0269614b30399101930588f2e424e
Cindy <cindyofwu@gmail.com>
2014-06-14 17:14:26 -0400

    for testing purposes

---
+++ Slime.java
@@ -0,0 +1,16 @@
+public class Slime extends Monster {
+    
+    public Slime() {
+	name = "Slime";
+	bhp = hp = 10;
+	bmp = mp = 0;
+	bstr = str = 2;
+	bwis = wis = 2;
+	bluck = luk = 1;
+	bspd = spd = 2;
+	bheight = height = 1;
+	bweight = weight = 1;
+	isDead = false;
+    }
+}
+	
