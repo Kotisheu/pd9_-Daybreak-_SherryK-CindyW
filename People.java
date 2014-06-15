5a1745552c2e25e89dbac0c6e2ae69c39dec0bf3
Cindy <cindyofwu@gmail.com>
2014-06-15 14:06:45 -0400

    Forgot to add =w=

--- People.java
+++ People.java
@@ -1,2 +1,32 @@
 public abstract class People extends Base{
+
+    public People() {
+	defing = false;
+	canAtk = true;
+	isDead = false;
+    }
+    
+    public void defend() {
+	def += 5;
+	defing = true;
+    }
+
+    public void setNormalDef() { //Normal defense after defending 
+	if ( defing ) { 
+	    def -= 5;
+	    defing = false;
+	}
+    }
+
+    public void gainExp( int i ) {
+	exp += i;
+	if ( exp > bexp ) {
+	    exp -= bexp; //new exp amount after lvlup
+	    bexp *= 1.5; // next exp increased
+	    lvlUp(); //stats increase
+	}
+    }
+    
+    public abstract void lvlUp(); //Each person has different boosts when lvl up
+
 }
