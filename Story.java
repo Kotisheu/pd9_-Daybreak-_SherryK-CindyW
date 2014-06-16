ac2ac87827f6cf14b7858477c7c36509e56ab487
Kotisheu <iiacherry@aim.com>
2014-06-16 02:51:12 -0400

    Create Story.java

---
+++ Story.java
@@ -0,0 +1,24 @@
+public class Story{
+    //hwototoimplemethiswheeeeee
+
+    //Towntexts
+    String t1=" You are at Kaetown. It is a  town with many inhabitants. The smell of delicious dishes wafts through the air.";
+    String t2= "You are at Seaporte. The bustling port city is full of people from various places. A cool salty breeze brushes against your skin.";
+    String t3=" You are at Town Tree. Gigantic trees tower over you, and tree houses many meters in the air can be seen. There is a calming sensation in the air.";
+    String t4=" You are at Metro. Skyscrapers surround you and fill you with awe. The scent of the city fills your nostrils.";
+    //Areatexts
+    String A1= "You are in Stonehenge. Despite it's name, it is only a grassland, without stones much bigger than a pebble to be seen.";
+    String A2=" You are in Feywood. You can hear animals in the distance chattering to each other under the cover of the darkness.";
+    String A3=" You are in Aguael. The splashing of the water surrounds you.";
+    String A4= " You are in Sans. The barren landscape fills you with a sense of loneliness.";
+    String A5=" You are in the Dessert Desert. You see desserts all around you, and can't help taking yet another bite.";
+    //eventtext? print on first time to place
+    //entry to seaporte
+    String E1=" As you arrive in Seaporte, you feel something is wrong. When you walk around, it suddenly clicks ! Seaporte has been over run by pirates!It's time to fight!";//startbattle
+    String E2= " You see a girl in the water crying for help.";//add option to save(aguael)
+    String E3=" You see a strange kangaroo wearing sunglasses in the town.";//addoptiontotalk(kaetown)
+    String E4=" You feel something swipe agaist your pocket and you turn, just in time to see someone making off with your wallet.";// Add option to chase, else lose money(Metro)
+    String E5=" One of the trees near you is considerably shorter than the others, as you watch it, you realize it has a face !";// Add option to approah( town tree)
+    String E6= " You finally arrive at the tower Wisarde lives in. You climb up the stair to see...";// if days> ?? he is dead :)
+	
+}
